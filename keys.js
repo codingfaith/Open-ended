2f07f32c360f81d72d649f0380666c566b437406
codingfaith <89830793+codingfaith@users.noreply.github.com>
2025-04-15 12:58:53 +0200

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,3 @@
+export const openaiConfig = {
+    apiKey: "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",
+}
