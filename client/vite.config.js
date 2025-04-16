d2a69a69f77ac32d0c5f5b4772ba1bd6658ebb21
codingfaith <codingfaith158@gmail.com>
2025-04-16 11:38:12 +0200

    Delete unnecessary junk like public folder

--- client/vite.config.js
+++
@@ -1,9 +0,0 @@
-// client/vite.config.js
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/', // Ensures paths work correctly on Netlify
-  build: {
-    outDir: 'dist', // Vite's default output folder
-  }
-})
