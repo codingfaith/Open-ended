59425c463dba0fc2b184151ad364b4ff042238fc
codingfaith <codingfaith158@gmail.com>
2025-04-24 17:20:52 +0200

    Add drag slider to select message for default sliders

--- client/index.js
+++ client/index.js
@@ -429,7 +429,7 @@ class UbuntexIndex {
             
             const valueDisplay = document.createElement("div");
             valueDisplay.className = "slider-value";
-            valueDisplay.textContent = `Selected: 5`;
+            valueDisplay.textContent = `Selected: 5 (drag slider to rate)`;
             
             slider.oninput = () => {
                 valueDisplay.textContent = `Selected: ${slider.value}`;
