d844e714b3ae7bd5ac23b1fc3a3ac451dc1af222
codingfaith <codingfaith158@gmail.com>
2025-04-29 23:10:56 +0200

    Update configs for quiz.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
@@ -7,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        landing: resolve(__dirname, 'quiz.html')
+        quiz: resolve(__dirname, 'quiz.html') 
       }
     }
   }
