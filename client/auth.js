30c8730fad3756be1ac6ed5e4587cda5834aecb0
codingfaith <codingfaith158@gmail.com>
2025-05-21 11:57:40 +0200

    Include window.env in supabase intialisation

--- client/auth.js
+++ client/auth.js
@@ -1,6 +1,6 @@
 // Initialize Supabase client using environment variables
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL || window.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY || window.env.SUPABASE_KEY;
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
