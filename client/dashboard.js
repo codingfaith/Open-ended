bdacdeef6d499fd66e3d41392473e9c0e8e226f1
codingfaith <codingfaith158@gmail.com>
2025-07-11 22:33:16 +0200

    add scroll message

--- client/dashboard.js
+++ client/dashboard.js
@@ -130,7 +130,7 @@ async function displayUserResults(userDocs, db) {
   if (!adminView) return;
   average = await calculateGlobalAverageScore(db);
   message.innerHTML = `<h3>${usersNum} users have taken the test!</h3>
-  Their average score is ${average}%`;
+  Their average score is ${average}% (<span style="font-size:small">scroll below</span>)`;
   
   // Clear existing content safely
   while (adminView.firstChild) {
