6a97e52b641430e670df3f75a05998afd5ae8297
codingfaith <codingfaith158@gmail.com>
2025-08-18 12:31:51 +0200

    link payment page redirect in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html'),
         contact: resolve(__dirname, 'contact.html'),
         auth: resolve(__dirname, 'auth.html'),
+        pay: resolve(__dirname, 'payment.html'),
         dashboard: resolve(__dirname, 'dashboard.html')
       }
     }
