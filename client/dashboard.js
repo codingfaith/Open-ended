2e80614d93b1e648ccd48e5a09720c6685b2334e
codingfaith <codingfaith158@gmail.com>
2025-10-02 14:32:36 +0200

    Update report

--- client/dashboard.js
+++ client/dashboard.js
@@ -299,7 +299,7 @@ function displayData(data) {
               See report
             </button>
             <div class="report-content hide" id="report-${index}">
-              <img src="Plus94_logo2.png" alt="Plus94 logo" class="p94_logo"/>
+              <img src="./Plus94_logo2.png" alt="Plus94 logo" class="p94_logo">
               <h1 id="report-name">Ubuntex Report for ${data.userProfile.firstName} ${data.userProfile.lastName}</h1>
               ${formatText(attempt.report)}
               <button class="downloadReportBtn">Download Report</button>
