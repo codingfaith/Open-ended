2a7548bd58583639e027ba1afcbcd870c06c9b27
codingfaith <codingfaith158@gmail.com>
2025-10-06 09:58:56 +0200

    Update report quality

--- client/dashboard.js
+++ client/dashboard.js
@@ -418,8 +418,7 @@ function displayAdminData(adminData) {
           <span class="attempt-date">Test Date: ${formatAttemptDate(attempt.timestamp)}</span>
           <span class="attempt-score">Test Score: ${attempt.score}%</span>
           <span class="attempt-class">Classification: ${attempt.classification}</span>
-        </div>
-        <h4 class="score-levels">(NB: Highest scores translate to Ubuntex Level 1, Lowest scores to Ubuntex Level 6)</h4>  
+        </div> 
         ${formatText(attempt.report)}
           <div class="ubuntex-classification">
             <p><strong>Ubuntex Classification Breakdown</strong></p>
