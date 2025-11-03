ddb58fe41b509d875a5391414477fd9d2faac8cc
codingfaith <codingfaith158@gmail.com>
2025-11-03 12:43:59 +0200

    Update image link

--- client/dashboard.js
+++ client/dashboard.js
@@ -297,9 +297,7 @@ function displayData(data) {
             </button>
             <div class="report-content hide" id="report-${index}">
               <div class="report-heading">
-                <img src="https://ubuntex.plus94.tech/assets/Plus94_logo2.png" 
-                onerror="this.onerror=null; this.src='https://ubuntex.netlify.app/assets/Plus94_logo2-CJbCr5Cg.png';" 
-                alt="Plus94 Logo" class="logo">
+                <img src="https://ubuntex.plus94.tech/assets/Plus94_logo2-CJbCr5Cg.png" alt="Plus94 Logo" class="logo">
                 <h1>Ubuntex</h1>
               </div>
               <h3 class="report-name">Ubuntu Index Report for ${data.userProfile.firstName} ${data.userProfile.lastName}</h3>
@@ -416,9 +414,7 @@ function displayAdminData(adminData) {
       </div>
       <div class="admin-report-content hide" id="admin-report-${index}">
         <div class="report-heading">
-           <img src="https://ubuntex.plus94.tech/assets/Plus94_logo2.png" 
-            onerror="this.onerror=null; this.src='https://ubuntex.netlify.app/assets/Plus94_logo2-CJbCr5Cg.png';" 
-            alt="Plus94 Logo" class="logo">
+          <img src="https://ubuntex.plus94.tech/assets/Plus94_logo2-CJbCr5Cg.png" alt="Plus94 Logo" class="logo">
           <h1>Ubuntex</h1>
         </div>
         <h3 class="report-name">Ubuntu Index Report for ${adminData.userProfile?.firstName} ${adminData.userProfile?.lastName}</h3>
